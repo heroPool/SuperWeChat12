e688517c4975b05a98010231a40b67164089977e
zhuHuaJian <935625755@qq>
2017-03-29 11:24:53 +0800

    修改Demo为SuperWeChat，添加resultutils，L，okhttpUtils，MD5，等工具类

--- SuperWeChat/src/cn/ucai/superwechat/ui/SplashActivity.java
+++ SuperWeChat/src/cn/ucai/superwechat/ui/SplashActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.util.EasyUtils;
 
-import cn.ucai.superwechat.DemoHelper;
+import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.R;
 
 /**
@@ -40,7 +40,7 @@ public class SplashActivity extends BaseActivity {
 
 		new Thread(new Runnable() {
 			public void run() {
-				if (DemoHelper.getInstance().isLoggedIn()) {
+				if (SuperWeChatHelper.getInstance().isLoggedIn()) {
 					// auto login mode, make sure all group and conversation is loaed before enter the main screen
 					long start = System.currentTimeMillis();
 					EMClient.getInstance().chatManager().loadAllConversations();
